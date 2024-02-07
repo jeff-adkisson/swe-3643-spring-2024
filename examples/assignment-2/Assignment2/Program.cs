a4d98b521ad825a6c9b7a8743a63ad8f94a5c6d8
Jeff Adkisson <68288613+jeff-adkisson@users.noreply.github.com>
2024-02-07 12:27:20 -0500

    Added notes

--- examples/assignment-2/Assignment2/Program.cs
+++ examples/assignment-2/Assignment2/Program.cs
@@ -1,9 +1,13 @@
-﻿double[] incomeList = new double[] { 111000d, 2000d, 15341.34d };
+﻿//EXAMPLE CALL - DOES *NOT* EXECUTE ALL PATHS!
+
+double[] incomeList = new double[] { 111000d, 2000d, 15341.34d };
 int[] parentList = new int[] { 27, 40 };
 int[] childList = new int[] { 13, 18 };
 
 double computedTax = ComputeTax(incomeList, parentList, childList);
 Console.WriteLine(computedTax);
+
+//END OF EXAMPLE CALL
 
 // incomeList[]: the array recording the individual income items
 // parentList[]: the array recording the ages of parents supported by this person
