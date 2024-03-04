6a8fd1530c51beaf5be5df6a41998a695a17fb88
Jeff Adkisson <68288613+jeff-adkisson@users.noreply.github.com>
2024-03-04 10:00:39 -0500

    Updated structure of example tests to AAA pattern

--- examples/assignment-4/test/edu/kennesaw/seclass/MyCustomStringTest.java
+++ examples/assignment-4/test/edu/kennesaw/seclass/MyCustomStringTest.java
@@ -13,6 +13,7 @@ public class MyCustomStringTest {
 
     @BeforeEach
     public void setUp() {
+        //arrange
         mycustomstring = new MyCustomString();
     }
 
@@ -22,149 +23,49 @@ public class MyCustomStringTest {
     }
 
     @Test
-    public void testCountNumbers1() {
+    public void MyCustomString_NumbersInString_Count_EXAMPLE() {
+
+        //arrange
         mycustomstring.setString("H3y, l3t'5 put s0me d161ts in this 5tr1n6!11!!");
-        assertEquals(9, mycustomstring.countNumbers());
+        int countOfNumbers = mycustomstring.countNumbers();
+
+        //assert
+        assertEquals(9,
+                countOfNumbers);
     }
 
     @Test
-    public void testCountNumbers2() {
-        fail("Not yet implemented");
-    }
+    public void MyCustomString_Phrase_Reverse_EXAMPLE() {
 
-    @Test
-    public void testCountNumbers3() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testCountNumbers4() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testCountNumbers5() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testCountNumbers6() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters1() {
+        //arrange
         mycustomstring.setString("Peter Piper picked a peck of pickled peppers.");
-        assertEquals("etePiP r repkcipa decep fo kcip delkpep srep.", mycustomstring.reverseNCharacters(4, false));
+
+        //act
+        String reversed = mycustomstring.reverseNCharacters(4, false);
+
+        //assert
+        assertEquals(
+                "etePiP r repkcipa decep fo kcip delkpep srep.",
+                reversed);
     }
 
     @Test
-    public void testReverseNCharacters2() {
-        mycustomstring.setString("Peter Piper picked a peck of pickled peppers.");
-        assertEquals("etePiP r repkcipa decep fo kcip delkpep srepXXX.", mycustomstring.reverseNCharacters(4, true));
-    }
-
-    @Test
-    public void testReverseNCharacters3() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters4() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters5() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters6() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters7() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters8() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters9() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters10() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testReverseNCharacters11() {
-        fail("Not yet implemented");
-    }
-
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring1() {
+    public void  MyCustomString_PhraseWithDigits_ConvertToDigits_EXAMPLE()
+    {
+        //arrange
         mycustomstring.setString("H3y, l3t'5 put 50me d161ts in this 5tr1n6!11!!");
+
+        //act
         mycustomstring.convertDigitsToNamesInSubstring(17, 24);
-        assertEquals("H3y, l3t'5 put 5Zerome dOnesixonets in this 5tr1n6!11!!", mycustomstring.getString());
+        String converted = mycustomstring.getString();
+
+        //asset
+        assertEquals(
+                "H3y, l3t'5 put 5Zerome dOnesixonets in this 5tr1n6!11!!",
+                converted);
     }
 
-    @Test
-    public void testConvertDigitsToNamesInSubstring2() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring3() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring4() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring5() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring6() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring7() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring8() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring9() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring10() {
-        fail("Not yet implemented");
-    }
-
-    @Test
-    public void testConvertDigitsToNamesInSubstring11() {
-        fail("Not yet implemented");
-    }
-
+//    public void MyCustomString_State_ExpectedBehavior() {
+//        fail("This test should fail");
+//    }
 }
